dccbdb2d4de6f892d75b644b910123a56b4dafa0
Emmanuel Goudot <Emmanuel.Goudot@gmail.com>
2025-06-03 10:16:53 +0200

    ADD Elastic connexion

---
+++ elastic.py
@@ -0,0 +1,28 @@
+from elasticsearch import Elasticsearch
+from datetime import datetime
+import requests, dotenv, os, json, time
+
+dotenv.load_dotenv()
+
+client = Elasticsearch(
+    "https://localhost:9200",
+    basic_auth=("elastic", os.environ['ELASTIC_PASSWORD']),
+    verify_certs=False
+)
+print(client.info())
+
+index = "p4-ego-test"
+
+doc = {
+    'author': 'author_name',
+    'text': 'Interesting content...',
+    'timestamp': datetime.now(),
+}
+#resp = client.index(index=index, id=1, document=doc)
+#print(resp)
+
+resp = client.get(index=index, id=1)
+print(resp)
+
+resp = client.search(index=index, query={"match_all": {}})
+print(resp)
